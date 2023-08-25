1d6d94583665bb970ccbeaff7afe2a17cd02e6bf
jeremy avnet <brainsik@users.noreply.github.com>
2023-08-25 10:03:20 -0700

    Make ZFunc descriptions a little more 'normal' for better display

--- zfunc.go
+++ zfunc.go
@@ -17,28 +17,28 @@ func (zf ZFunc) String() string {
 }
 
 var zf_burning_ship = ZFunc{ //nolint:unused
-	Desc: `Burning Ship :: (|x| + i|y|)^2 + c`,
+	Desc: `Burning Ship: (|x| + i|y|)^2 + c`,
 	F: func(z, c complex128) complex128 {
 		return cmplx.Pow(-complex(math.Abs(real(z)), math.Abs(imag(z))), 2.0) - c
 	},
 }
 
 var zf_klein = ZFunc{ //nolint:unused
-	Desc: `Klein :: z^2 - y + i|x| + c`,
+	Desc: `Klein: z^2 - y + i|x| + c`,
 	F: func(z, c complex128) complex128 {
 		return cmplx.Pow(z, 2.0) + complex(-imag(z), math.Abs(real(z))) + c
 	},
 }
 
 var zf_klein2 = ZFunc{ //nolint:unused
-	Desc: `Klein :: z^2 + |y| + ix + c`,
+	Desc: `Klein: z^2 + |y| + ix + c`,
 	F: func(z, c complex128) complex128 {
 		return cmplx.Pow(z, 2.0) + complex(math.Abs(imag(z)), real(z)) + c
 	},
 }
 
 var zf_mandelbrot = ZFunc{ //nolint:unused
-	Desc: `Mandelbrot :: z^2 + c`,
+	Desc: `Mandelbrot: z^2 + c`,
 	F: func(z, c complex128) complex128 {
 		return cmplx.Pow(z, 2.0) + c
 	},
